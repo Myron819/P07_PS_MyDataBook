6c2344799259ed29680b8dd58b7e363f6ce108c3
Myron819 <19036849@rp.edu.sg>
2021-06-03 11:02:10 +0800

    Initial commit

---
+++ app/src/main/java/c302_p06_sakilaclient/webservices/rp/edu/p07psmydatabook/MainActivity.java
@@ -0,0 +1,14 @@
+package c302_p06_sakilaclient.webservices.rp.edu.p07psmydatabook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/c302_p06_sakilaclient/webservices/rp/edu/p07psmydatabook/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package c302_p06_sakilaclient.webservices.rp.edu.p07psmydatabook;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
